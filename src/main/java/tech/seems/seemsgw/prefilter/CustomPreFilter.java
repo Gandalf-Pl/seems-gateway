0558ecf147046a8a2117c1d58a77651bf310876c
seems <529574002@qq.com>
2023-08-12 17:25:22 +0800

    add filter

--- src/main/java/tech/seems/seemsgw/prefilter/CustomPreFilter.java
+++ src/main/java/tech/seems/seemsgw/prefilter/CustomPreFilter.java
@@ -1,6 +1,5 @@
-package tech.seems.seemsgw.postfilter;
+package tech.seems.seemsgw.prefilter;
 
-import org.springframework.boot.web.servlet.filter.OrderedFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.stereotype.Component;
@@ -8,17 +7,11 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
-public class PostCustomFilter implements GlobalFilter, OrderedFilter {
+public class CustomPreFilter implements GlobalFilter{
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return chain.filter(exchange).then(
-                Mono.fromRunnable(() -> System.out.println("post filter ---- "))
-        );
-    }
-
-    @Override
-    public int getOrder() {
-        return 100;
+        System.out.println("pre filter ------");
+        return chain.filter(exchange);
     }
 }
